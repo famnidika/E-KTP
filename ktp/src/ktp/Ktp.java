6a0780c68183d2af2cbe54925f334c54e176c159
ACER <ACER@laptop>
2017-05-19 20:04:25 -0700

    class pertama

--- ktp/src/ktp/Ktp.java
+++ ktp/src/ktp/Ktp.java
@@ -11,12 +11,52 @@ package ktp;
  */
 public class Ktp {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("hai");
+   private String Nama;
+   private String TTL;
+   private String Jekel;
+   private String Goldar;
+   private String Alamat;
+   private String Agama;
+   private String Status;
+   private String Pekerjaan;
+   private String Kewarganegaraan;
+   public Ktp(String Nama, String TTL, String Jekel, String Goldar, String Alamat, String Agama, String Status, String Pekerjaan, String Kewarganegaraan) {
+    this.Nama = Nama;
+    this.TTL = TTL;
+    this.Jekel = Jekel;
+    this.Goldar = Goldar;
+    this.Alamat = Alamat;
+    this.Agama = Agama;
+    this.Status = Status;
+    this.Pekerjaan = Pekerjaan;
+    this.Kewarganegaraan = Kewarganegaraan;
     }
+   public String getNama(){
+       return Nama;
+   }
+   public String getTTL(){
+       return TTL;
+   }
+   public String getJekel(){
+       return Jekel;
+   }
+   public String getGoldar(){
+       return Goldar;
+   }
+   public String getAlamat(){
+       return Alamat;
+   }
+   public String getAgama(){
+       return Agama;
+   }
+   public String getStatus(){
+       return Status;
+   }
+   public String getPekerjaan(){
+       return Pekerjaan;
+   }
+   public String getKewarganegaraan(){
+       return Kewarganegaraan;
+   }
     
 }
